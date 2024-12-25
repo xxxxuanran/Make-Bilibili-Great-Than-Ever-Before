21de28cff0604dc19e5852d70927527a3981435b
SukkaW <isukkaw@gmail.com>
2024-12-25 11:10:17 +0800

    refactor(use-system-fonts): update selector

--- src/modules/use-system-fonts.ts
+++ src/modules/use-system-fonts.ts
@@ -6,7 +6,7 @@ const useSystemFonts: MakeBilibiliGreatThanEverBeforeModule = {
   name: 'use-system-fonts',
   description: '去除鸿蒙字体，强制使用系统默认字体',
   any({ addStyle }) {
-    document.querySelectorAll(String.raw`link[href*=\/font\/]`).forEach(x => x.remove());
+    document.querySelectorAll('link[href*="/jinkela/long/font/"]').forEach(x => x.remove());
     addStyle(css`html, body { font-family: initial !important; }`);
   }
 };
