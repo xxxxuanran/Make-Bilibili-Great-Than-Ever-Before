e2d0dce29b128c0f9de144eeb8fe0be997c95724
SukkaW <isukkaw@gmail.com>
2024-12-25 20:17:25 +0800

    feat(optimize-homepage): bypass bilibili adguard check

--- src/modules/optimize-homepage.ts
+++ src/modules/optimize-homepage.ts
@@ -5,7 +5,26 @@ const optimizeHomepage: MakeBilibiliGreatThanEverBeforeModule = {
   name: 'optimize-homepage',
   description: '首页广告去除和样式优化',
   any({ addStyle }) {
-    addStyle(css`.feed2 .feed-card:has(a[href*="cm.bilibili.com"]), .feed2 .feed-card:has(.bili-video-card:empty) { display: none } .feed2 .container > * { margin-top: 0 !important }`);
+    addStyle(css`
+      .feed2 .feed-card:has(a[href*="cm.bilibili.com"]),
+      .feed2 .feed-card:has(.bili-video-card:empty) {
+        width: 1px !important;
+        height: 1px !important;
+        opacity: 0 !important;
+        pointer-events: none !important;
+        position: absolute !important;
+        padding: 0 !important;
+        margin: -1px !important;
+        overflow: hidden !important;
+        clip: rect(0, 0, 0, 0) !important;
+        white-space: nowrap !important;
+        border-width: 0 !important;
+      }
+
+      .feed2 .container > * {
+        margin-top: 0 !important
+      }
+    `);
   }
 };
 
