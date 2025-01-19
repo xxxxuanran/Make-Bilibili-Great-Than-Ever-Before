3ffe7a3040680c1a9e6be4d144318e140564d5f2
Pig Fang <g-plane@hotmail.com>
2025-01-19 14:24:42 +0800

    feat: reset font to `system-ui` (#7)

--- src/modules/use-system-fonts.ts
+++ src/modules/use-system-fonts.ts
@@ -7,7 +7,7 @@ const useSystemFonts: MakeBilibiliGreatThanEverBeforeModule = {
   description: '去除鸿蒙字体，强制使用系统默认字体',
   any({ addStyle }) {
     document.querySelectorAll('link[href*="/jinkela/long/font/"]').forEach(x => x.remove());
-    addStyle(css`html, body { font-family: initial !important; }`);
+    addStyle(css`html, body { font-family: system-ui !important; }`);
   }
 };
 
